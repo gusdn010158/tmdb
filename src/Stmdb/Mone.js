326d8f223acd419f3125403327817bc5bbeb0ab7
gusdn010158 <gusdn010158@gmail.com>
2024-11-13 00:57:12 +0900

    weffgd

--- src/Stmdb/Mone.js
+++ src/Stmdb/Mone.js
@@ -1,8 +1,63 @@
-import React from "react";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
 function Mone(props) {
-  return <div>주간 사이트 기여자 - 기간 : November 05-11</div>;
+  const [Mone, setMone] = useState();
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/Mone")
+      .then((response) => {
+        setMone(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div>
+      <Mon>
+        <h2>주간 사이트 기여자 - 기간 : November 05-11</h2>
+        {Mone.map((item, index) => (
+          <Mfuc>
+            <Mhthr>{item.num}</Mhthr>
+            <Mimg src={item.img} alt={index} />
+            <div>
+              <Mhtwo>{item.name}</Mhtwo>
+              <Mhnine>{item.text}</Mhnine>
+            </div>
+          </Mfuc>
+        ))}
+      </Mon>
+    </div>
+  );
 }
 
 export default Mone;
+const Mon = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+const Mhthr = styled.div`
+  font-size: 20px;
+`;
+const Mimg = styled.img`
+  background-color: black;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  margin: 10px;
+`;
+const Mfuc = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Mhtwo = styled.div`
+  font-weight: 600;
+  font-size: 18px;
+`;
+const Mhnine = styled.div`
+  font-weight: 200;
+  font-size: 13px;
+`;
